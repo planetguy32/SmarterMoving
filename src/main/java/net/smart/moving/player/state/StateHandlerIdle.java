a52ddea214c2c458580010eed89bc300b7ffb186
planetguy32 <adampprice@google.com>
2020-06-29 21:51:32 -0700

    Refactor out states into separate classes
    Also moves code generally into classes per movement state. (There are still scattered places outside State* that behave differently per state, but much of the state-specific code is collected. Client-side code is generally ignored in this refactor - .)

---
+++ src/main/java/net/smart/moving/player/state/StateHandlerIdle.java
@@ -0,0 +1,13 @@
+package net.smart.moving.player.state;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.smart.moving.player.PlayerConstants;
+
+public class StateHandlerIdle extends StateHandler {
+
+    public void afterOnUpdate(EntityPlayer player) {
+        player.height = PlayerConstants.DEFAULT_HEIGHT;
+        player.eyeHeight = player.getDefaultEyeHeight();
+    }
+
+}
